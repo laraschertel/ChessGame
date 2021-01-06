6f98ff986aba813b585721dab46a28bbae83d885
lrnbr21 <laraschertel@gmail.com>
2021-01-06 10:15:00 +0100

    improved commit

---
+++ src/network/ProtocolEngine.java
@@ -0,0 +1,26 @@
+package network;
+
+import chess.StatusException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public interface ProtocolEngine {
+    /**
+     * Setup protocol engine. Provide streams to read from and write to.
+     *
+     * @param is
+     * @param os
+     * @throws IOException
+     */
+    void handleConnection(InputStream is, OutputStream os) throws IOException;
+
+    /**
+     * Stop engine - close streams and release all resources
+     *
+     * @throws IOException
+     */
+    void close() throws IOException;
+
+}
